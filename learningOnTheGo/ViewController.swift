e4e5b5bbee14c8396ee05892d732b8c2a6ebb5c8
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-08-31 22:15:02 -0400

    standard UI set up

---
+++ learningOnTheGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  learningOnTheGo
+//
+//  Created by XIN on 8/31/20.
+//  Copyright © 2020 XIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
